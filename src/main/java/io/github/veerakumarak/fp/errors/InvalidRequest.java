71c42e9fc381c012796674bfe171f2c01793b3fe
Veera Kumar <veerakumarak@gmail.com>
2025-06-20 21:22:24 +0530

    invalid reasosns added

--- src/main/java/io/github/veerakumarak/fp/errors/InvalidRequest.java
+++ src/main/java/io/github/veerakumarak/fp/errors/InvalidRequest.java
@@ -2,8 +2,22 @@ package io.github.veerakumarak.fp.errors;
 
 import io.github.veerakumarak.fp.BaseError;
 
+import java.util.List;
+import java.util.Map;
+
 public class InvalidRequest extends BaseError {
+    private final Map<String, List<String>> reasons;
     public InvalidRequest(final String message) {
         super(message);
+        reasons = null;
+    }
+
+    public InvalidRequest(Map<String, List<String>> reasons) {
+        super(null);
+        this.reasons = reasons;
+    }
+
+    public Map<String, List<String>> getReasons() {
+        return reasons;
     }
 }
